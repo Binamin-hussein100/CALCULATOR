05ac9cc7ca0bb093b881e278f2d59022aaaf222d
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-06-17 08:02:40 +0300

    separated business logic and user interface logic

---
+++ calc.js
@@ -0,0 +1,26 @@
+var add = function(num1,num2){
+    return num1 + num2;
+};
+
+var subtract = function(num1, num2){
+    return num1 - num2;
+};
+
+var multiply  = function(num1, num2){
+    return num1 * num2;
+};
+
+var division = function(num1, num2){
+    return num1 / num2;
+};
+
+// user interface or frontend logic.
+$(document).ready(function(){
+$("form#add").submit(function(){
+event.preventDefault();
+var num1 = parseInt($("#add1").val());
+var num2 = parseInt($("#add2").val());
+var result = add(num1, num2)
+$("#output").text(result)
+});
+});
